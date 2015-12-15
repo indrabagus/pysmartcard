312c7e587f6d5315ac54a9a3588044bbda4efadc
Indra Bagus <indra.bagus@gmail.com>
2015-12-15 16:40:17 +0700

    Rename example of getstatus changed

--- Tests/ex_getstatuschange.py
+++ Tests/ex_getstatuschange.py
@@ -3,7 +3,7 @@ import sys
 
 ctx= _scard.context()
 print("Get status change...")
-lstatus = ctx.get_status_change(2000)
+lstatus = ctx.get_status_change(20000)
 for i in lstatus:
     print("Current status : %X" % (i.currentstate))
     print("Eventstate: %X" % (i.eventstate))
