aa95a0fc9100a15227aade8007e6bff2b314e2d2
Indra Bagus <indra.bagus@gmail.com>
2016-08-02 16:10:33 +0700

    print every event

--- Tests/ex_getstatuschange.py
+++ Tests/ex_getstatuschange.py
@@ -1,11 +1,18 @@
-import _scard
+﻿import _scard
 import sys
 
 ctx= _scard.context()
 print("Get status change...")
 lstatus = ctx.get_status_change(0xFFFFFFFF)
 for i in lstatus:
-    print("Current status : %X" % (i.currentstate))
-    print("Eventstate: %X" % (i.eventstate))
+    print("Current status : 0x%X" % (i.currentstate))
+    print("Eventstate: 0x%X" % (i.eventstate))
+    if((i.eventstate & _scard.SCARD_STATE_CHANGED) == _scard.SCARD_STATE_CHANGED):
+        print("Scard state changed")
+    if((i.eventstate & _scard.SCARD_STATE_PRESENT) == _scard.SCARD_STATE_PRESENT):
+        print("Scard state present")
+    if((i.eventstate & _scard.SCARD_STATE_EXCLUSIVE) == _scard.SCARD_STATE_EXCLUSIVE):
+        print("SCARD state Exclusive")
+    print()
 print("exiting..")
 sys.exit(0)
